fd0b48285c47da19085b3569430b5c7def70f6a9
Felipe <alemaof@hotmail.com>
2024-01-11 13:25:31 -0300

    Módulo 8

---
+++ mod8/CalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,14 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+
+        double nota1 = 10;
+        double nota2 = 8.5;
+        double nota3 = 9;
+        double nota4 = 5;
+        double media;
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A media e : " + media);
+    }
+}
