3a0b07d84de32a0d5551d3061546929226ee94ce
Felipe <alemaof@hotmail.com>
2024-02-06 13:58:14 -0300

    Módulo 37

---
+++ mod37/DependenciasMaven/src/main/java/br/com/ffrantz/App.java
@@ -0,0 +1,13 @@
+package br.com.ffrantz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
